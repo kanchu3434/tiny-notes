a7726ca8e61e749f1107898dbb3f6e72de9d212c
kanchan <kanchankotkar001@gmail.com>
2022-12-10 07:25:58 +0530

    readme added note

---
+++ src/view/Addnote/Addnote.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Addnote.css";
+
+
+function Addnote() {
+    return (
+      <>
+      <h1> This is Addnotepage</h1>
+      </>
+    );
+  }
+  
+  export default Addnote;
